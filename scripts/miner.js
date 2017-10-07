4cbbf7c29aa79b94cb45982f75dfcd073cdb7e3c
Carl Furrow <carl.furrow@gmail.com>
2017-10-07 15:25:54 -0400

    Squashed commit of the following:
    commit 1560002e3660179135f68c38764ee6142a591522
    Author: Carl Furrow <carl.furrow@gmail.com>
    Date:   Sun Sep 24 21:57:09 2017 -0400
    
        Use currentAnim to set action frame dir

--- scripts/miner.js
+++ scripts/miner.js
@@ -4,6 +4,12 @@ Miner = function(game, x, y) {
   this.anchor.set(0.5, 0.8);
   this.smoothed = false;
 
+  this.controller       = this.game.input.keyboard.createCursorKeys();
+  this.controller.space = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+
+  this.xWalkFrame = null;
+  this.yWalkFrame = null;
+
   game.add.existing(this);
 };
 
@@ -16,82 +22,101 @@ Miner.preload = function(game) {
   game.load.spritesheet('mine', 'assets/gold-mine.png', 96, 96);
 };
 
-Miner.actions = {
+Miner.facing = {
   UP:    1,
   RIGHT: 2,
   DOWN:  4,
   LEFT:  8,
-  ATTACK: 16
+
+};
+Miner.action = {
+  WALK: 1,
+  ATTACK: 2
 };
 
-Miner.prototype.update = function(){
+Miner.prototype.update = function() {
   var speed = 200;
-  // What if we did bitwise ops?
-  // 0 => nothing
-  // 1 => UP
-  // 2 => RIGHT
-  // 4 => DOWN
-  // 8 => LEFT
-  this.currentAction = 0; // nothing
 
-  if(this.game.input.keyboard.isDown(Phaser.KeyCode.UP)) {
-    this.body.velocity.y = -speed;
-    this.currentAction |= Miner.actions.UP;
-  }
-  if(this.game.input.keyboard.isDown(Phaser.KeyCode.DOWN)) {
-    this.body.velocity.y = speed;
-    this.currentAction |= Miner.actions.DOWN;
-  }
-  if(this.game.input.keyboard.isDown(Phaser.KeyCode.LEFT)) {
-    this.body.velocity.x = -speed;
+  if(this.controller.left.isDown) {
+    this.action = 'walk';
     this.scale.x = -Math.abs(this.scale.x);
-    this.currentAction |= Miner.actions.LEFT;
-  }
-  if(this.game.input.keyboard.isDown(Phaser.KeyCode.RIGHT)) {
-    this.body.velocity.x = speed;
+    this.body.velocity.x = -speed;
+    this.xWalkFrame = this.xActionFrame = 'right';
+  } else if(this.controller.right.isDown) {
+    this.action = 'walk';
     this.scale.x = Math.abs(this.scale.x);
-    this.currentAction |= Miner.actions.RIGHT;
+    this.body.velocity.x = speed;
+    this.xWalkFrame = this.xActionFrame = 'right';
+  } else {
+    this.xWalkFrame = null;
+    this.body.velocity.x = 0;
   }
 
-  if(this.game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)) {
-    this.currentAction |= Miner.actions.ATTACK;
+  if(this.controller.up.isDown) {
+    this.action = 'walk';
+    this.body.velocity.y = -speed;
+    this.yWalkFrame = this.yActionFrame = 'up';
+  }else if(this.controller.down.isDown) {
+    this.action = 'walk';
+    this.body.velocity.y = speed;
+    this.yWalkFrame = this.yActionFrame = 'down';
+  } else {
+    this.yWalkFrame = null;
+    this.body.velocity.y = 0;
   }
 
-  if(this.currentAction === 0) {
-    this.animations.stop();
+  if(!this._directionKeyIsDown()) {
+    this.action = null;
+  }
+
+  if(this.controller.space.isDown) {
     this.body.velocity.x = 0;
     this.body.velocity.y = 0;
-  } else {
-    this.animate();
+    this.action = 'attack';
   }
+
+  this.animate();
+};
+
+Miner.prototype._directionKeyIsDown = function() {
+  return this._leftRightIsDown() ||
+         this._upDownIsDown();
+};
+
+Miner.prototype._leftRightIsDown = function() {
+  return this.controller.left.isDown ||
+         this.controller.right.isDown;
+};
+
+Miner.prototype._upDownIsDown = function() {
+  return this.controller.up.isDown ||
+         this.controller.down.isDown;
 };
 
 Miner.prototype.animate = function() {
   var directionKey = [];
   var animationKey = null;
-  var action = 'walk';
 
-  if(this.currentAction === 0) {
+  if(this.action === null) {
+    this.animations.stop();
     return;
   }
 
-  if(this.currentAction & Miner.actions.UP) {
-    directionKey.push('up');
-  }
-  if(this.currentAction & Miner.actions.DOWN) {
-    directionKey.push('down');
-  }
-  if(this.currentAction & Miner.actions.RIGHT || this.currentAction & Miner.actions.LEFT) {
-    directionKey.push('right');
+  if(this.yWalkFrame) {
+    directionKey.push(this.yWalkFrame);
   }
 
-  if(this.currentAction & Miner.actions.ATTACK) {
-    action = 'attack';
+  if(this.xWalkFrame) {
+    directionKey.push(this.xWalkFrame);
   }
 
-  directionKey = directionKey.join('-');
-  animationKey = action + '-' + directionKey;
-  console.log(animationKey);
+  if(this.action == 'attack') {
+    directionKey = this.animations.currentAnim.name.replace(/^(walk|attack)-/, '');
+  } else {
+    directionKey = directionKey.join('-');
+  }
+  animationKey = this.action + '-' + directionKey;
+
   this.animations.play(animationKey);
 };
 
