b61108188cc8705e41495f4a689521d97e9e9d52
Carl Furrow <carl.furrow@gmail.com>
2017-10-07 21:32:10 -0400

    Move commented code into Level class

--- scripts/game.js
+++ scripts/game.js
@@ -41,61 +41,27 @@ function create() {
   var bloodAnimation = this.blood.animations.add('squirt', [0,5,10], 5, false);
   bloodAnimation.onComplete.add(function(sprite, animation){ sprite.visible=false; }, this);
 
-  //this.game.physics.arcade.enable([this.mine]);
-
   this.miner.body.collideWorldBounds = true;
   this.miner.body.allowDrag = true;
   this.miner.body.setSize(72 / this.miner.scale.x, 72 / this.miner.scale.y, 72 / this.miner.scale.x, 72 / this.miner.scale.y);
 
-  // this.mine.body.immovable = true;
-  // this.mine.body.setSize(40, 40, 16, 48);
-
   this.game.camera.follow(this.miner);
-
-  exitGoldMine.call(this);
 }
 
-function mineSomeGold() {
-  //this.mine.frame = 1;
-  //enterGoldMine.call(this);
-}
-
-function enterGoldMine() {
-  this.mine.kill();
-  //this.group.kill();
-  this.miner.position.set(550, 200);
-  // this.layer.destroy();
-  // this.layer = this.map.createLayer('Gold Mine');
-  this.map.setCollisionByExclusion([83,84,85,99,100,101,112,113,128,129,144,145,160,161,115,131,147,148]);
-  // this.layer.setScale(3);
-  // this.layer.sendToBack();
-  // this.layer.resizeWorld();
-}
-
-function exitGoldMine() {
-  //this.mine bring back to life
-  // this.layer.destroy();
-  // this.layer = this.map.createLayer('Overworld');
-  // this.layer.scale.set(3);
-  // this.layer.sendToBack();
-}
-
-function attackConfusion() {
-  if(!this.miner.attacking) {
-    this.blood.visible = false;
-    return;
-  }
-  this.blood.visible = true;
-  var bloodAnimation = this.blood.animations.getAnimation('squirt');
-  if(!bloodAnimation.isPlaying) {
-    this.blood.animations.play('squirt');
-  }
-}
+// function attackConfusion() {
+//   if(!this.miner.attacking) {
+//     this.blood.visible = false;
+//     return;
+//   }
+//   this.blood.visible = true;
+//   var bloodAnimation = this.blood.animations.getAnimation('squirt');
+//   if(!bloodAnimation.isPlaying) {
+//     this.blood.animations.play('squirt');
+//   }
+// }
 
 function update() {
   var speed = 200;
-  //this.mine.frame = 0; // reset to "off"
-
   //this.game.physics.arcade.collide(this.miner, this.mine, mineSomeGold, null, this);
   this.game.physics.arcade.collide(this.miner, this.level.layer);
 
--- scripts/level.js
+++ scripts/level.js
@@ -1,8 +1,8 @@
 Level = function(game, key){
   this.game = game;
   this.key = key;
-  this.tileWidth = 16;
-  this.tileHeight = 16;
+  this.tileWidth = 48;
+  this.tileHeight = 48;
 
   this.map = game.add.tilemap('cave');
   this.map.addTilesetImage('cave3x');
@@ -11,6 +11,11 @@ Level = function(game, key){
   this.layer.resizeWorld();
 
   this.map.createFromObjects('Carl', 'Gold Mine', 'gold-mine3x', 0);
+
+  // this.mine.body.immovable = true;
+  // this.mine.body.setSize(40, 40, 16, 48);
+  //
+  //this.game.physics.arcade.enable([this.mine]);
 };
 
 Level.preload = function(game) {
@@ -18,3 +23,29 @@ Level.preload = function(game) {
   game.load.image('cave3x', 'assets/levels/cave3x.png');
   game.load.spritesheet('gold-mine3x', 'assets/gold-mine.png', 96, 96);
 };
+
+function mineSomeGold() {
+  //this.mine.frame = 1;
+  //enterGoldMine.call(this);
+}
+
+function enterGoldMine() {
+  this.mine.kill();
+  //this.group.kill();
+  this.miner.position.set(550, 200);
+  // this.layer.destroy();
+  // this.layer = this.map.createLayer('Gold Mine');
+  this.map.setCollisionByExclusion([83,84,85,99,100,101,112,113,128,129,144,145,160,161,115,131,147,148]);
+  // this.layer.setScale(3);
+  // this.layer.sendToBack();
+  // this.layer.resizeWorld();
+}
+
+function exitGoldMine() {
+  //this.mine bring back to life
+  // this.layer.destroy();
+  // this.layer = this.map.createLayer('Overworld');
+  // this.layer.scale.set(3);
+  // this.layer.sendToBack();
+}
+exitGoldMine.call(this);
